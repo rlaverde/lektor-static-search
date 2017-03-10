ca42a0f33af9daf9e8ff8f6663c0bce5c2360406
Rafael Laverde <leafar91@gmail.com>
2017-03-10 12:43:30 -0500

    Some changes to the Readme.

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup
 setup(
     name='lektor-static-search',
     version='0.1',
-    author=u'Rafael Laverde,,,',
+    author=u'Rafael Laverde',
     author_email='leafar91@gmail.com',
+    url='https://github.com/rlaverde/lektor-static-search',
     license='MIT',
     py_modules=['lektor_static_search'],
     entry_points={
